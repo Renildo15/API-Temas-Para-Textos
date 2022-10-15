42e184f86c75191287d6da9a9ac2ad0f617bdf42
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-10-15 17:38:42 -0300

    Rodando a API

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const app = express
+const app = express();
+const port = process.env.PORT || 3000;
+const temas = require("./src/temas/temas.json");
 
-
-app.get("/temas", (req,res)=>{
-    return res.json()
+app.get("/temas",(req,res) => {
+    return res.json(temas);
 })
+
+app.listen(port, ()=>{
+    console.log("Servidor rodando!")
+});
